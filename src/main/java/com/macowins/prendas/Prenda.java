587d6fb62df783f9a84dde9230ec9d895f191bd4
Gaspar Habif <gasparhabif@gmail.com>
2021-04-14 20:16:10 -0300

    Added tipoDePrenda to Class Prenda

--- src/main/java/com/macowins/prendas/Prenda.java
+++ src/main/java/com/macowins/prendas/Prenda.java
@@ -5,6 +5,7 @@ import com.macowins.prendas.estados.EstadoPrenda;
 public class Prenda {
   private double valor;
   private EstadoPrenda estado;
+  private String tipoDePrenda;
 
   public Prenda(double valor, EstadoPrenda estado) {
     this.valor = valor;
@@ -25,5 +26,13 @@ public class Prenda {
 
   public void setValor(double valor) {
     this.valor = valor;
+  }
+
+  public String getTipoDePrenda() {
+    return tipoDePrenda;
+  }
+
+  public void setTipoDePrenda(String tipoDePrenda) {
+    this.tipoDePrenda = tipoDePrenda;
   }
 }
